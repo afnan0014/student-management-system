78250f144285e1ed431cef143fab5750d6e36590
Deepakdev pk <deepxxk999@gmail.com>
2025-05-19 10:44:47 +0530

    add course

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.department.name})"
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    semester = models.IntegerField()
+    staff = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, limit_choices_to={'groups__name': 'Staff'})
+
+    def __str__(self):
+        return f"{self.name} - Sem {self.semester} ({self.course.name})"
