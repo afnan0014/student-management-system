c05bbd5eb85399fb18509cb25f0a11095aa61ab0
RABEEH009 <rabeeh0129@gmail.com>
2025-05-29 18:58:15 +0530

    Added a landing page

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import landing_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('', landing_view, name='landing'),
+    path('accounts/', include('accounts.urls')),
     path('attendance/', include('attendance.urls')),
     path('courses/', include('courses.urls')),
     path('reports/', include('reports.urls')),  # Reports URL included properly
